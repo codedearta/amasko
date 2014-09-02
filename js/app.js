1a8b2f5b638a288dbfca28996b147a3e122e4d76
My-Yen Ngo <My-Yen@my-yens-air.home>
2014-09-02 23:21:05 +0100

    First commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
+
+// Code goes here
+
+var Amasko = angular.module('Amasko', []). // route configuration of view and controllers
+config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+    when('/Behandlungen', {
+        templateUrl: 'pages/Behandlungen.html',
+        controller: AmaskoController
+    }).
+    when('/Gutscheine', {
+        templateUrl: 'pages/Gutscheine.html',
+        controller: AmaskoController
+    }).
+    when('/Shop', {
+        templateUrl: 'pages/Shop.html',
+        controller: AmaskoController
+    }).
+    when('/Kontakt', {
+        templateUrl: 'pages/Kontakt.html',
+        controller: AmaskoController
+    }).
+    otherwise({
+        templateUrl: 'pages/Home.html',
+        controller: AmaskoController
+    });
+}]);
+
+
+function AmaskoController($scope) {
+  $scope.name = 'Welcome to Amasko';
+}
+
 $(document).foundation();
