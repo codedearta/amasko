f9bc4b2ee017bf9ece456f3bb675d62efc819c48
Sepp Renfer <sepp@macbook-pro.home>
2014-09-14 19:19:07 +0100

    texte hinzugefuegt

--- js/app.js
+++ js/app.js
@@ -6,10 +6,14 @@ config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
     when('/Behandlungen', {
         templateUrl: 'pages/Behandlungen.html',
-        controller: AmaskoController
-    }).
+        controller: BehandlungenCtrl
+    }).  
     when('/Gutscheine', {
         templateUrl: 'pages/Gutscheine.html',
+        controller: AmaskoController
+    }).
+    when('/Gallerie', {
+        templateUrl: 'pages/Gallerie.html',
         controller: AmaskoController
     }).
     when('/Shop', {
@@ -29,6 +33,24 @@ config(['$routeProvider', function ($routeProvider) {
 
 function AmaskoController($scope) {
   $scope.name = 'Welcome to Amasko';
+}
+
+function BehandlungenCtrl($scope) {
+  $scope.name = 'Welcome to Amasko';
+    $scope.templates =
+      [ { name: 'Gesicht.html', url: 'pages/Behandlungen/Gesicht.html'},
+        { name: 'Haende_und_Fuesse.html', url: 'pages/Behandlungen/Haende_und_Fuesse.html'},
+        { name: 'Haarentfernung.html', url: 'pages/Behandlungen/Haarentfernung.html'},
+        { name: 'Wellness.html', url: 'pages/Behandlungen/Wellness.html'},
+        { name: 'Cellulite.html', url: 'pages/Behandlungen/Cellulite.html'},
+        { name: 'Fettreduktion.html', url: 'pages/Behandlungen/Fettreduktion.html'},
+        { name: 'Powerplate.html', url: 'pages/Behandlungen/Powerplate.html'} 
+      ];
+    $scope.template = $scope.templates[0];
+    
+    $scope.selectSubCategory = new function(name) {
+        $scope.template = _.find($scope.templates, { 'name': name });
+    }
 }
 
 $(document).foundation();
