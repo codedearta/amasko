c0ec1cdd02b3ca8259b2dea79321f79425286bbf
Sepp Renfer <sepp@macbook-pro.home>
2014-09-14 22:29:43 +0100

    navigation fixed

--- js/app.js
+++ js/app.js
@@ -1,34 +1,105 @@
 
 // Code goes here
 
-var Amasko = angular.module('Amasko', []). // route configuration of view and controllers
-config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.
-    when('/Behandlungen', {
-        templateUrl: 'pages/Behandlungen.html',
-        controller: BehandlungenCtrl
-    }).  
-    when('/Gutscheine', {
-        templateUrl: 'pages/Gutscheine.html',
-        controller: AmaskoController
-    }).
-    when('/Gallerie', {
-        templateUrl: 'pages/Gallerie.html',
-        controller: AmaskoController
-    }).
-    when('/Shop', {
-        templateUrl: 'pages/Shop.html',
-        controller: AmaskoController
-    }).
-    when('/Kontakt', {
-        templateUrl: 'pages/Kontakt.html',
-        controller: AmaskoController
-    }).
-    otherwise({
-        templateUrl: 'pages/Home.html',
-        controller: AmaskoController
+var Amasko = angular.module('Amasko', ['ui.router']). // route configuration of view and controllers
+//config(['$routeProvider', function ($routeProvider) {
+//    $routeProvider.
+//    when('/Behandlungen', {
+//        templateUrl: 'pages/Behandlungen.html',
+//        controller: BehandlungenCtrl
+//    }).    
+//    when('/Behandlungen/Gesicht', {
+//        templateUrl: 'pages/Behandlungen.html',
+//        controller: BehandlungenCtrl
+//    }).
+//    when('/Behandlungen/Haende_und_Fuesse', {
+//        templateUrl: 'pages/Behandlungen.html',
+//        controller: BehandlungenCtrl
+//    }).  
+//       
+//    when('/Gutscheine', {
+//        templateUrl: 'pages/Gutscheine.html',
+//        controller: AmaskoController
+//    }).
+//    when('/Gallerie', {
+//        templateUrl: 'pages/Gallerie.html',
+//        controller: AmaskoController
+//    }).
+//    when('/Shop', {
+//        templateUrl: 'pages/Shop.html',
+//        controller: AmaskoController
+//    }).
+//    when('/Kontakt', {
+//        templateUrl: 'pages/Kontakt.html',
+//        controller: AmaskoController
+//    }).
+//    otherwise({
+//        templateUrl: 'pages/Behandlungen.html',
+//        controller: AmaskoController
+//    });
+//}]);
+
+config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/Home");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('Home', {
+      url: '/Home',
+      templateUrl: 'pages/Home.html'
+    })
+    .state('Behandlungen', {
+      url: '/Behandlungen',
+      templateUrl: 'pages/Behandlungen.html'
+    })
+    .state('Behandlungen.Gesicht', {
+      url: "/Behandlungen/Gesicht",
+      templateUrl: "pages/Behandlungen/Gesicht.html",
+    })
+    .state('Behandlungen.Haende_und_Fuesse', {
+      url: "/Behandlungen/Haende_und_Fuesse",
+      templateUrl: "pages/Behandlungen/Haende_und_Fuesse.html",
+    })
+    .state('Behandlungen.Haarentfernung', {
+      url: "/Behandlungen/Haarentfernung",
+      templateUrl: "pages/Behandlungen/Haarentfernung.html",
+    })
+    .state('Behandlungen.Wellness', {
+      url: "/Behandlungen/Wellness",
+      templateUrl: "pages/Behandlungen/Wellness.html",
+    })
+    .state('Behandlungen.Cellulite', {
+      url: "/Behandlungen/Cellulite",
+      templateUrl: "pages/Behandlungen/Cellulite.html",
+    })
+    .state('Behandlungen.Fettreduktion', {
+      url: "/Behandlungen/Fettreduktion",
+      templateUrl: "pages/Behandlungen/Fettreduktion.html",
+    })
+    .state('Behandlungen.Powerplate', {
+      url: "/Behandlungen/Powerplate",
+      templateUrl: "pages/Behandlungen/Powerplate.html",
+    })  
+  
+    .state('Gutscheine', {
+      url: "/Gutscheine",
+      templateUrl: "pages/Gutscheine.html"
+    })
+    .state('Gallerie', {
+      url: '/Gallerie',
+      templateUrl: 'pages/Gallerie.html'
+    })
+    .state('Shop', {
+      url: '/Shop',
+      templateUrl: 'pages/Shop.html'
+    })
+    .state('Kontakt', {
+      url: '/Kontakt',
+      templateUrl: 'pages/Kontakt.html'
     });
-}]);
+});
 
 
 function AmaskoController($scope) {
