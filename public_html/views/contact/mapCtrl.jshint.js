e03a40702099179fc668752455ad80407d20f4b8
My-Yen Ngo <myyen.ngo@gmail.com>
2016-04-14 22:15:28 +0100

    Test

--- public_html/views/contact/mapCtrl.jshint.js
+++ public_html/views/contact/mapCtrl.jshint.js
@@ -1,4 +1,5 @@
 QUnit.module('JSHint - views/contact');
 QUnit.test('views/contact/mapCtrl.js should pass jshint', function(assert) { 
+  assert.expect(1);
   assert.ok(true, 'views/contact/mapCtrl.js should pass jshint.'); 
 });
